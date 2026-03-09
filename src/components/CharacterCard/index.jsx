ac07124538967823eb782d8085093360554b8780
AndreWill99 <andrewilliam.cbpm@gmail.com>
2026-03-09 15:01:35 -0300

    feat: create SearchBar component with search input, filter, and order dropdowns.

--- src/components/CharacterCard/index.jsx
+++ src/components/CharacterCard/index.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import './style_card.css'; // O CSS que define o card com bordas arredondadas
 
+
 const CharacterCard = ({ name, image }) => {
+  const imageUrl = image.url.replace("http://", "https://");
   return (
     <div className="character-card">
       <div className="image-container">
-        <img src={image} alt={name} className="character-image" />
+        <img src={imageUrl} alt={name} className="character-image" referrerPolicy="no-referrer" />
       </div>
       <p className="character-name">{name}</p>
     </div>
